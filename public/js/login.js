f3a4b1784476fcab69c1a1a9b7f48d807e7fb3b5
realwangshuxin <1137758203@qq.com>
2019-07-11 21:12:54 +0800

    更新

--- public/js/login.js
+++ public/js/login.js
@@ -1,28 +1,67 @@
 $(function(){
-    var reg1=/^\w{6,}$/;
+    var reg1=/^\w{5,}$/;
     var reg2=/^\d{6}$/;
     $("#uname").blur(function(){
-        if($("#uname").val()==""){
-            $(".error1").attr("src","../img/login/login_file.png")
+        var $uname=$("#uname").val();
+        if($uname==""){
+            $(".error1").attr("src","../img/login/login_file.png");
+        }
+        if($uname!=""){
+            if(reg1.test($uname)){
+                $(".error1").attr("src","../img/login/login_ss.png")
+            }else{
+                $(".error1").attr("src","../img/login/login_file.png")
+            }
         }
     })
     $("#upwd").blur(function(){
-        if($("#upwd").val()==""){
+        var $upwd=$("#upwd").val(); 
+        if($upwd==""){
             $(".error2").attr("src","../img/login/login_file.png")
         }
+        if($upwd!=""){
+            if(reg2.test($upwd)){
+                $(".error2").attr("src","../img/login/login_ss.png")
+            }else{
+                $(".error2").attr("src","../img/login/login_file.png")
+            }
+        }
     })
+    var verifyCode = new GVerify("v_container");
     $("#code_input").blur(function(){
+        var res = verifyCode.validate($("#code_input").val());
         if($("#code_input").val()==""){
-            $(".error2").attr("src","../img/login/login_file.png")
+            $(".error3").attr("src","../img/login/login_file.png")
+            return;
+        }
+        if($("#code_input").val()!=""){
+            if(res){
+                $(".error3").attr("src","../img/login/login_ss.png")
+            }else{
+                $(".error3").attr("src","../img/login/login_file.png")
+                return;
+            }
         }
     })
-    $.ajax({
-        url:"http://127.0.0.1:8888/login",
-        type:"get",
-        data:{uid:1},
-        dataType:"json"
-    }).then(result=>{
-        console.log(result)
 
+    $("#my_button").click(function(){
+        var uname=$("#uname").val();
+        var upwd=$("#upwd").val();
+        $.ajax({
+            url:"http://127.0.0.1:8888/login",
+            type:"get",
+            data:{uname,upwd},
+            dataType:"json"
+        }).then(result=>{
+            // result=JSON.parse(result);
+            console.log(result)
+            if(result.code==1){
+                $(".yes").css({"width":"478px","transition":"all .5s linear"})
+            }
+            if(result.code==-1){
+                $(".no").css({"width":"478px","transition":"all .5s linear"})
+            }
+    
+        })
     })
 })
--- zh/routes/login.js
+++ zh/routes/login.js
@@ -2,14 +2,16 @@ const express=require("express")
 const router=express.Router();
 const pool=require("../pool");
 router.get("/",(req,res)=>{
-    var uid=req.query.uid;
-    var sql=`SELECT * FROM zh_login WHERE uid=?`;
-    pool.query(sql,[uid],(err,result)=>{
+    var uname=req.query.uname;
+    var upwd=req.query.upwd;
+    
+    var sql=`SELECT * FROM zh_login WHERE uname=? AND upwd=?`;
+    pool.query(sql,[uname,upwd],(err,result)=>{
         if(err)throw err;
         if(result.length>0){
-            res.send("登录成功");
+            res.send({code:1,msg:"登录成功"});
         }else{
-            res.send("登录失败")
+            res.send({code:-1,msg:"登录失败"})
         }
     })
 })
